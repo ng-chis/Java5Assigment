6e18c52a04caa834d6b61cf76d60e521b33dd134
longnntpc07608 <150583615+longnntpc07608@users.noreply.github.com>
2024-06-07 15:06:33 +0700

    Add files via upload

---
+++ src/main/java/com/poly/Java5AssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java5AssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java5AssignmentApplication.class, args);
+	}
+
+}
